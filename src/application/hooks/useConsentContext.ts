97425cf7627488839923b14809399a4940e8c391
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-04-20 09:04:57 +0700

    📝 fix: improve error handling in useConsentContext for better context validation
    - Added braces to the error handling logic to enhance readability and maintainability.
    - Ensured that an error is thrown only when the context is not available.

--- src/application/hooks/useConsentContext.ts
+++ src/application/hooks/useConsentContext.ts
@@ -3,12 +3,14 @@ import { ConsentContext } from "../utils/ConsentContext"
 
 export const useConsentContext = () => {
   const ctx = useContext(ConsentContext)
-  if (!ctx)
+  if (!ctx) {
     console.error(
       "useConsentContext must be used within <CookieConsentProvider>"
     )
-  throw new Error(
-    "useConsentContext must be used within <CookieConsentProvider>"
-  )
+    throw new Error(
+      "useConsentContext must be used within <CookieConsentProvider>"
+    )
+  }
+
   return ctx
 }
